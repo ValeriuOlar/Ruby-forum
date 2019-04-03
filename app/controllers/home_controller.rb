b81a966b797c48d500a90ccfeda2ad94df912c99
Valeriu Olariu <ovaleriu7@gmail.com>
2019-04-03 08:29:01 +0000

    Stock API integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @stock = StockQuote::Stock.quote("symbol")
+
+  if params [:id] == ""
+    @nothing = "Hey, you forgot to eneter symbol"
+elsif
+
+if params[:id]
+  @stock = StockQuote::Stock.qoute(params[:id])
+  end
+    end
+    
   end
   
   def about
